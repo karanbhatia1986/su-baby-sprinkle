2ffc9c8400cfcf509f9b13f4ed053a478f967be8
karanbhatia1986 <37342562+karanbhatia1986@users.noreply.github.com>
2026-05-08 18:38:37 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,31 +1,31 @@
 const triviaQuestions = [
     {
         id: 1,
-        question: "What weighed more as a baby?",
-        options: ["Karan", "Su"],
-        correctAnswer: null
-    },
-    {
-        id: 2,
-        question: "What sleeps more like a baby?",
-        options: ["Karan", "Su"],
-        correctAnswer: null
-    },
-    {
-        id: 3,
-        question: "Who will change the most diapers?",
-        options: ["Karan", "Su"],
-        correctAnswer: null
-    },
-    {
-        id: 4,
         question: "Who told their family first about their baby?",
         options: ["Karan", "Su"],
         correctAnswer: null
     },
     {
-        id: 5,
+        id: 2,
+        question: "What weighed more as a baby?",
+        options: ["Karan", "Su"],
+        correctAnswer: null
+    },
+    {
+        id: 3,
         question: "Who will make the baby laugh first?",
+        options: ["Karan", "Su"],
+        correctAnswer: null
+    }
+    {
+        id: 4,
+        question: "What sleeps more like a baby?",
+        options: ["Karan", "Su"],
+        correctAnswer: null
+    },
+    {
+        id: 5,
+        question: "Who will change the most diapers?",
         options: ["Karan", "Su"],
         correctAnswer: null
     }
