e777ceb9df30dcab172721eff539b00f20ac980c
karanbhatia1986 <37342562+karanbhatia1986@users.noreply.github.com>
2026-05-17 13:14:02 -0400

    Delete mommy-best directory

--- mommy-best/questions.js
+++
@@ -1,8 +0,0 @@
-const triviaQuestions = [
-    {
-        id: 1,
-        question: "Who told their family first about their baby?",
-        options: ["Karan", "Su"],
-        correctAnswer: null
-    }
-];
