b69df0ca66e867f2d32fa9277945731aecc20746
karanbhatia1986 <37342562+karanbhatia1986@users.noreply.github.com>
2026-05-17 12:53:59 -0400

    Create questions.js

---
+++ mommy-best/questions.js
@@ -0,0 +1,8 @@
+const triviaQuestions = [
+    {
+        id: 1,
+        question: "Who told their family first about their baby?",
+        options: ["Karan", "Su"],
+        correctAnswer: null
+    }
+];
